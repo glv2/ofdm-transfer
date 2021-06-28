bdf31b877660c0c80d0c398df5aa6a30950b16d2
Guillaume Le Vaillant <glv@posteo.net>
2021-06-28 18:25:46 +0200

    Reduce gain

--- ofdm-transfer.c
+++ ofdm-transfer.c
@@ -375,7 +375,7 @@ void send_frames(radio_t *radio, float sample_rate, unsigned int bit_rate,
         }
         /* Reduce the amplitude of samples because the frame generator
          * may produce samples with an amplitude greater than 1.0 */
-        liquid_vectorcf_mulscalar(frame_samples, n, 0.22, frame_samples);
+        liquid_vectorcf_mulscalar(frame_samples, n, 0.2, frame_samples);
         msresamp_crcf_execute(resampler, frame_samples, n, samples, &n);
         if(frequency_offset != 0)
         {

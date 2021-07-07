3ff44590bd1ad7fe3fb5f359a60387d6aa377705
Guillaume Le Vaillant <glv@posteo.net>
2021-07-07 14:32:41 +0200

    Add documentation for API functions

--- src/ofdm-transfer.h
+++ src/ofdm-transfer.h
@@ -23,8 +23,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 typedef struct ofdm_transfer_s *ofdm_transfer_t;
 
+/* Set the verbosity level
+ *  - v: if not 0, print some debug messages to stderr
+ */
 void ofdm_transfer_set_verbose(unsigned char v);
+
+/* Get the verbosity level */
 unsigned char ofdm_transfer_is_verbose();
+
+/* Initialize a new transfer
+ *  - radio_driver: radio to use (e.g. "io" or "driver=hackrf")
+ *  - emit: 1 for transmit mode; 0 for receive mode
+ *  - file: in transmit mode, read data from this file
+ *          in receive mode, write data to this file
+ *          if NULL, use stdin or stdout instead
+ *  - sample_rate: samples per second
+ *  - bit_rate: bits per second
+ *  - frequency: center frequency of the transfer in Hertz
+ *  - frequency_offset: set the frequency of the radio frequency_offset Hz
+ *    lower than the frequency of the transfer (can be negative to set the
+ *    frequency higher)
+ *  - gain: gain of the radio transceiver
+ *  - ppm: correction for the radio clock
+ *  - subcarrier_modulation: modulation to use for the subcarriers
+ *  - subcarriers: number of subcarriers
+ *  - cyclic_prefix_length: length of the cyclic prefix
+ *  - taper_length: length of the taper
+ *  - inner_fec: inner forward error correction code to use
+ *  - outer_fec: outer forward error correction code to use
+ *  - id: transfer id; when receiving, frames with a different id will be
+ *        ignored
+ *  - dump: if not NULL, write raw samples sent or received to this file
+ */
 ofdm_transfer_t ofdm_transfer_create(char *radio_driver,
                                      unsigned char emit,
                                      char *file,
@@ -42,11 +72,24 @@ ofdm_transfer_t ofdm_transfer_create(char *radio_driver,
                                      char *outer_fec,
                                      char *id,
                                      char *dump);
+
+
+/* Cleanup after a finished transfer */
 void ofdm_transfer_free(ofdm_transfer_t transfer);
+
+/* Start a transfer and return when finished */
 void ofdm_transfer_start(ofdm_transfer_t transfer);
+
+/* Interrupt a transfer */
 void ofdm_transfer_stop(ofdm_transfer_t transfer);
+
+/* Interrupt all transfers */
 void ofdm_transfer_stop_all();
+
+/* Print list of detected software defined radios */
 void ofdm_transfer_print_available_radios();
+
+/* Print list of supported forward error codes */
 void ofdm_transfer_print_available_forward_error_codes();
 
 #endif
